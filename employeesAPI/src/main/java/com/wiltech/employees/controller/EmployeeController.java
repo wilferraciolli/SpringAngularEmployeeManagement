6f0db285257a9307bbd3adf96ff989afaeba86f3
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-02-26 20:38:38 +0000

    Added Cross Origin accept all

--- employeesAPI/src/main/java/com/wiltech/employees/controller/EmployeeController.java
+++ employeesAPI/src/main/java/com/wiltech/employees/controller/EmployeeController.java
@@ -12,19 +12,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ * The type Employee controller.
+ */
 @RestController
+@CrossOrigin
 @RequestMapping("/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    /**
+     * Gets all employees.
+     *
+     * @return the all employees
+     */
     @GetMapping()
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    /**
+     * Gets employee by id.
+     *
+     * @param employeeId the employee id
+     * @return the employee by id
+     * @throws ResourceNotFoundException the resource not found exception
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
             throws ResourceNotFoundException {
@@ -33,11 +48,25 @@ public class EmployeeController {
         return ResponseEntity.ok().body(employee);
     }
 
+    /**
+     * Create employee employee.
+     *
+     * @param employee the employee
+     * @return the employee
+     */
     @PostMapping()
     public Employee createEmployee(@Valid @RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    /**
+     * Update employee response entity.
+     *
+     * @param employeeId      the employee id
+     * @param employeeDetails the employee details
+     * @return the response entity
+     * @throws ResourceNotFoundException the resource not found exception
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
                                                    @Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
@@ -51,6 +80,13 @@ public class EmployeeController {
         return ResponseEntity.ok(updatedEmployee);
     }
 
+    /**
+     * Delete employee map.
+     *
+     * @param employeeId the employee id
+     * @return the map
+     * @throws ResourceNotFoundException the resource not found exception
+     */
     @DeleteMapping("/{id}")
     public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
             throws ResourceNotFoundException {
