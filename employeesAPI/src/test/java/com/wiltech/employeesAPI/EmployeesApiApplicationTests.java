0c42b5172a088ecd9617d15057e008af10365230
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-02-26 19:19:38 +0000

    Created API project

---
+++ employeesAPI/src/test/java/com/wiltech/employeesAPI/EmployeesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wiltech.employeesAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
